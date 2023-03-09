4bb11aead44d88409f43c151074fb7e488fb1c2b
oldevelopment <robin@orangeandlemons.nl>
2023-03-09 11:52:01 +0100

    second step to unit testing

--- src/test/utils.test.ts
+++ src/test/utils.test.ts
@@ -1,8 +1,43 @@
-import { toUpperCase } from "../app/utils";
+import { getStringInfo, toUpperCase } from "../app/utils";
+
+const actual = getStringInfo("My-string");
 
 describe("Utils test suite", () => {
-  test("should return uppercase", () => {
+  it("should return uppercase of valid string", () => {
     const result = toUpperCase("abc");
     expect(result).toBe("ABC");
+  });
+});
+
+describe("getStringInfo should", () => {
+  test("return lowercase", () => {
+    expect(actual.lowerCase).toBe("my-string");
+  });
+  test("return character length", () => {
+    expect(actual.characters.length).toBe(9);
+    expect(actual.characters).toHaveLength(9);
+    expect(actual.extraInfo).toEqual({});
+  });
+  test("return character to equal", () => {
+    expect(actual.characters).toEqual([
+      "M",
+      "y",
+      "-",
+      "s",
+      "t",
+      "r",
+      "i",
+      "n",
+      "g",
+    ]);
+  });
+  test("return character to contain", () => {
+    expect(actual.characters).toContain<string>("M");
+    expect.arrayContaining(["s", "t", "r", "i", "n", "g", "M", "y", "-"]);
+  });
+  test("return defined", () => {
+    expect(actual.extraInfo).not.toBe(undefined);
+    expect(actual.extraInfo).not.toBeUndefined();
+    expect(actual.extraInfo).toBeTruthy();
   });
 });
